768a47911913cf5dc70dc99d76166ad85289a5d3
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-01-28 11:11:21 -0400

    trying all tasks

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uppercase(s):
+    for char in s:
+        if 97 <= ord(char) <= 122:
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print("")
