59f9a0f3251d0cee5cb187a9fda7299d347be3f4
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-01-31 17:32:56 -0400

    task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+    n_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
 
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            new_matrix[i][j] = matrix[i][j] ** 2
+            n_matrix[i][j] = matrix[i][j] ** 2
 
-    return new_matrix
+    return n_matrix
