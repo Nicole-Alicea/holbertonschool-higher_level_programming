8e598115e38501ce52067dd517d5bf2f625510c5
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-01-31 20:12:10 -0400

    fixing typo

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,5 +3,5 @@ def multiply_by_2(a_dictionary):
     multiplied_dictionary = {}
 
     for key, value in a_dictionary.items():
-        multipied_dictionary[key] = value * 2
+        multiplied_dictionary[key] = value * 2
     return multiplied_dictionary
