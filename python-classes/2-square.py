05656ba0639bb2ae4cf7e3411d90e15b1bebf0ec
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-04 17:00:23 -0400

    fixing typo

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,9 +5,9 @@
 class Square:
     """Class that defines a square with a private instance attribute."""
     def __init__(self, size=0):
-        if not isinstance(value, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = value
+            self.__size = size
