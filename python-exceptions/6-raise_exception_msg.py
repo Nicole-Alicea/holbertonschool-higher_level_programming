062dcab610b6f82191ae946714b460892f2a24be
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-03 23:57:13 -0400

    task 6

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    try:
-        undefined_variable
-    except NameError as e:
-        raise NameError(f"{message}: {e}")
+    raise NameError(message)
