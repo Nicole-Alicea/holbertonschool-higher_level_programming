d82c0e8d64c3c753ec66c0c710170fef71d0fae5
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-03 22:31:20 -0400

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
     try:
+        elements_printed = 0
         for i in range(x):
-            print(my_list[i], end=' ')
-            count += 1
-    except IndexError:
-        pass
-    finally:
+            print(my_list[i], end="")
+            elements_printed += 1
         print()
-    return count
+        return elements_printed
+    except IndexError:
+        print("Out of range")
+        return x - elements_printed
