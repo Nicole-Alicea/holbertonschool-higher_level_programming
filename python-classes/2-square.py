0485e612fa954e672a9ceb5d5ba780d00b29e152
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-04 16:50:58 -0400

    task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This will create a class named square that defines a square."""
+
+
+class Square:
+    """Class that defines a square with a private instance attribute."""
+    def __init__(self, size=0):
+        self.__size = 0
+        self.size = size
+
+    @property
+    def size(self)
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
