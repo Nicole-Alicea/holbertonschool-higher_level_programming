86789e2485d19a3a61b62fda91059000fda22360
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-03 20:52:49 -0400

    fixing code

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
     roman_numerals = {
             'I': 1,
             'V': 5,
@@ -14,7 +17,7 @@ def roman_to_int(roman_string):
     prev_value = 0
 
     for numeral in reversed(roman_string):
-        value = roman_numerals[numeral]
+        value = roman_numerals.get(numeral, 0)
 
         if value < prev_value:
             total -= value
