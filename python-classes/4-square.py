0d7b6fad75b1f0f2b7b8f675c0c43d296656e238
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-04 18:06:24 -0400

    task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,6 +3,7 @@
 
 
 class Square:
+    """Class that represents a square"""
     def __init__(self, size=0):
         self.size = size
 
@@ -14,7 +15,7 @@ class Square:
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
