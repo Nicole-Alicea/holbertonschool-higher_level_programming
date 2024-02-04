61f0bc15f024e233f38f20debc3c30b22711c5a1
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-04 17:48:36 -0400

    task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,6 +3,7 @@
 
 
 class Square:
+    """Class that represents a square"""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
