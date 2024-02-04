c3d7dc6bd8aab7749900812e7008aad2dbbac051
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-04 18:31:49 -0400

    task 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -15,14 +15,16 @@ class Square:
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         return self.__size ** 2
 
     def my_print(self):
-        if size == 0:
-            print("--")
-        else:
-            print(area)
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
