3470dd8f72a94c46dfbc936e391dd8a1ffa811f9
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-04 16:57:10 -0400

    task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,11 +8,6 @@ class Square:
         self.__size = 0
         self.size = size
 
-    @property
-    def size(self)
-        return self.__size
-
-    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
