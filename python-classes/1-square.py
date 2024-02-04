24fb20772ede746537e7865f2511faf808b29b19
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-04 15:04:52 -0400

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This will create a class named square that defines a square."""
+
+
+class Square:
+    """Class with a private instance attribute."""
+    def __init__(self, size):
+        self.__size = size
