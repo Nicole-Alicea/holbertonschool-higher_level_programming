d6fad7d1fb8a7de2c0b330626bca77b0699913d0
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-04 17:01:34 -0400

    fixing typo

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,7 +7,6 @@ class Square:
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
