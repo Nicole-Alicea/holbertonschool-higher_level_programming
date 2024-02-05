509c575f68a3b71b997bbc10e8718e97b69cbd6d
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-04 23:48:37 -0400

    task 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This function prints 'My name is <first name> <last name>'"""
 
+
 def say_my_name(first_name, last_name="")
     """Prints a formatted string with the given first and last name"""
 
