c09241d415551b63bb9e5d1445c2f18bd22fbb37
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-05 21:01:09 -0400

    adding to code

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """Creates a class with an attribute for width and height"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -14,10 +14,11 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -25,7 +26,8 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(value):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
