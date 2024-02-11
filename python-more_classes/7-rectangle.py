6e9ea53e59b5c970b4d1a980aff28d3f66f99b09
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-11 14:02:11 -0400

    task 7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -53,7 +53,7 @@ class Rectangle:
             rectangle_str = ""
             for i in range(self.__height):
                 rectangle_str += str(self.print_symbol) * self.__width + '\n'
-                return rectangle_str[:-1]
+            return rectangle_str[:-1]
 
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
