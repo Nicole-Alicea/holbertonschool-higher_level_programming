68825cddd0696ccb28b8c02900a2ee383f35a2f4
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-11 16:44:47 -0400

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Creates a class"""
+
+
+class BaseGeometry:
+    """contains a public instance method"""
+    def area(self):
+        raise Exception("area() is not implemented")
