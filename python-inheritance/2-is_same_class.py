99fcd0ac2df76c53e32e29fd6715025d8b6cf86c
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-11 15:44:18 -0400

    task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Creates a function"""
 
+
 def is_same_class(obj, a_class):
     """Will return True if object is exactly an instance of the
         specified class; otherwise False
