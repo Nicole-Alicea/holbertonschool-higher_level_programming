3e3a9caa7064cd857498fae3b45280851aefadb3
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-11 18:10:30 -0400

    task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Creates a function that returns the JSON representation of an object"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Will return the JSON representation of an object(string)"""
+    return json.dumps(my_obj)
