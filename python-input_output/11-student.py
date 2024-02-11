1a5cc2d9ba048e78bfc66222d71d64049f45965a
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-11 18:55:27 -0400

    task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Creates a class that defines a student"""
+
+
+class Student:
+    """Class with attributes and methods"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        Student_dict = vars(self)
+        wanted_data = {}
+        if isinstance(attrs, list):
+            for key in attrs:
+                if isinstance(key, str) and key in Student_dict:
+                    wanted_data[key] = Student_dict[key]
+            return wanted_data
+        return Student_dict
+
+    def reload_from_json(self, json):
+        self.first_name = json.get("first_name", "J")
+        self.last_name = json.get("last_name", "S")
+        self.age = json.get("age", 1)
