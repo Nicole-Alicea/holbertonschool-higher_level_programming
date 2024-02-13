d38df168b40106df3e48764b43a70f3e182f1f2c
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-13 13:16:19 -0400

    trying task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """Creates a class that will inherit from the 'base' class"""
-
-
 from models.base import Base
 
 
 class Rectangle(Base):
     """Derived class with its attributes"""
 
-    def __init__(self, width, height, x=0, y=0, id=None):   
+    def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.__width = width
         self.__height = height
