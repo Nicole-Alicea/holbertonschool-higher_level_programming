c5cd59afc5dd77f071bbf3326838f872d2dad3d3
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-13 09:17:17 -0400

    trying task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,11 +7,10 @@ from models.base import Base
 
 class Rectangle(Base):
     """Derived class with its attributes"""
-    def __init__(self, width, height, x=0, y=0, id=None):
-       
-        super().__init__(id)
-       
 
+
+    def __init__(self, width, height, x=0, y=0, id=None):   
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
