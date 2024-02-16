a3e031891213c740dbaa06fec8f365138110c214
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-16 09:00:37 -0400

    trying task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,6 +19,6 @@ class Base:
         """Returns the JSON string representation of
             list_dictionaries
         """
-        if list_dictionaries is None or list_dictionaries == "":
+        if list_dictionaries is None or list_dictionaries == []:
             return '"[]"'
         return json.dumps(list_dictionaries)
