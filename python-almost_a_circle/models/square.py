d893978d87edbc12511b24e6cd00ac200b3e5fc6
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-15 23:05:26 -0400

    trying task 10

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,7 +11,8 @@ class Square(Rectangle):
 
     def __str__(self):
         """Returns a string representation of Square"""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
