81052e2ce110bcfef00ce0048e711fd682400b9a
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-15 22:51:08 -0400

    trying task 14

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,7 +8,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Class constructor"""
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     @property
     def size(self):
@@ -23,8 +22,8 @@ class Square(Rectangle):
 
     def __str__(self):
         """Returns a string representation of Square"""
-        s = "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.size)
+        s = "[{}] ({}) {}/{} - {}".format(
+            type(self).__name__, self.id, self.x, self.y, self.width)
         return s
 
     def update(self, *args, **kwargs):
@@ -44,9 +43,5 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         """Returns the dictionary representation of a Square"""
-        return {
-            'id': self.id,
-            'size': self.size,
-            'x': self.x,
-            'y': self.y
-        }
+        return {"id": self.id, "size": self.width,
+                "x": self.x, "y": self.y}
