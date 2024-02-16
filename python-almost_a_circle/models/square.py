c680136386fe67203fa71715bbaaa6f41dcb1239
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-15 22:20:37 -0400

    trying task 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,6 +10,17 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
         self.size = size
 
+    @property
+    def size(self):
+        """gets size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """sets the same value for width and height"""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Returns a string representation of Square"""
         s = "[Square] ({}) {}/{} - {}".format(
