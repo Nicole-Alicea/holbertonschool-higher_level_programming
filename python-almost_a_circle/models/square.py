5bbf7b5f003eef211557836245640c172cf097ac
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-15 22:35:52 -0400

    trying task 13

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -41,3 +41,12 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
