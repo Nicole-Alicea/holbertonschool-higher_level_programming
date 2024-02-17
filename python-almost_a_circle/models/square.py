4f4735abe0b6ea2d22d1944685d194547dbad140
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-17 12:44:49 -0400

    adding more documentation

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,9 +4,17 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Class inheriting from Rectangle"""
+    """Class representing a square, inheriting from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-        """Class constructor"""
+        """
+            Class constructor - Initializes a Square object
+
+            Parameters:
+            - size (int): The size of the square.
+            - x (int): The x-coordinate of the square (default is 0).
+            - y (int): The y-coordinate of the square (default is 0).
+            - id: The identifier of the square (default is None).
+        """
         super().__init__(size, size, x, y, id)
         self.size = size
 
@@ -27,7 +35,23 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
-        """Assigns arguments to attributes"""
+        """
+            Updates the attributes of the square.
+
+            Accepts either positional arguments or keyword arguments.
+
+            If positional arguments are used:
+            - First argument (optional): id
+            - Second argument (optional): size
+            - Third argument (optional): x
+            - Fourth argument (optional): y
+
+            If keyword arguments are used:
+            - id (optional): The identifier of the square.
+            - size (optional): The size of the square.
+            - x (optional): The x-coordinate of the square.
+            - y (optional): The y-coordinate of the square.
+        """
         if args:
             if len(args) >= 1:
                 self.id = args[0]
