1ede5c3c5adaadbee9a588cb43e40fe0a1dd5174
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-18 22:11:32 -0400

    adding test

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -10,3 +10,12 @@ class TestSquareMethods(unittest.TestCase):
     def setUp(self):
         """Set up a default Square instance for testing"""
         self.default_square = Square(size=3, id=1)
+
+    def test_default_values(self):
+        """Test default values of the Square instance"""
+        self.assertEqual(self.default_square.size, 3)
+        self.assertEqual(self.default_square.width, 3)
+        self.assertEqual(self.default_square.height, 3)
+        self.assertEqual(self.default_square.x, 0)
+        self.assertEqual(self.default_square.y, 0)
+        self.assertEqual(self.default_square.id, 1)
