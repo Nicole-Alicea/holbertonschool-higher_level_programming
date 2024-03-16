262c9e5dc2afef8443e10a3ea0b156c618fedcf6
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-03-16 17:57:31 -0400

    task 6 fixing

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,7 @@
 -- Script that creates a database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-id INT UNIQUE AUTO NOT NULL PRIMARY KEY,
-name VARCHAR(256) NOT NULL);
+id INT UNIQUE AUTO_INCREMENT NOT NULL,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY (id)
+);
