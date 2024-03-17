9682c48a974507b473ecd5c7f19927f65a0dd28d
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-03-16 23:05:47 -0400

    task 11 fixing

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,5 @@
 -- Script that lists all shows contained in the database 'hbtn_0d_tvshows'
 SELECT tv_shows.title, IF NULL (tv_show_genres.genre_id, 'NULL') AS genre_id
-FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
