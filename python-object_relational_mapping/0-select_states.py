f59756500e06738a67b9c1bc4c9714e599406bf0
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-03-19 14:18:45 -0400

    trying task 0 again, different approach

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,59 +1,20 @@
 #!/usr/bin/python3
 '''This script connects to a MySQL database and lists all of the states from the database hbtn_0e_0_usa'''
 import MySQLdb
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 import sys
 
 
-Base = declarative_base()
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf8")
+    
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
 
+    rows = cursor.fetchall()
+    for x in rows:
+        print(x)
 
-class State(Base):
-    '''Represents the structure of the "states" table'''
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(256))
-
-def list_states(username, password, database):
-    '''Connects to the MySQL database and lists states'''
-    mysql_conn = None
-    session = None
-
-    try:
-        mysql_conn = MySQLdb.connect(host="localhost",
-                                     port=3306,
-                                     user=username,
-                                     passwd=password,
-                                     db=database)
-        
-        engine = create_engine(
-            f"mysql://{username}:{password}@localhost:3306/{database}")
-
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        
-        states = session.query(State).order_by(State.id).all()
-        for state in states:
-            print((state.id, state.name))
-
-    except Exception as e:
-        print("Error:", e)
-
-    finally:
-        if mysql_conn:
-            mysql_conn.close()
-        
-        if session:
-            session.close()
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    list_states(username, password, database)
+    cursor.close()
+    db.close()
