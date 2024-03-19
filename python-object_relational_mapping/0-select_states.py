8b8f3df196188d3d0676642a51f86dcb1016d3bd
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-03-19 14:24:52 -0400

    adding empy lines between code lines

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,6 +2,8 @@
 '''This script connects to a MySQL database and lists all of the
     states from the database hbtn_0e_0_usa
 '''
+
+
 import MySQLdb
 import sys
 
@@ -11,10 +13,13 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], charset="utf8")
+    
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id")
+    
     rows = cursor.fetchall()
     for x in rows:
         print(x)
+    
     cursor.close()
     db.close()
