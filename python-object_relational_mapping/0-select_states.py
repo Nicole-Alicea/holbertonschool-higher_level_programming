958301100febb81836a67c1f3a02d71cfaff1545
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-03-19 14:22:01 -0400

    pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-'''This script connects to a MySQL database and lists all of the states from the database hbtn_0e_0_usa'''
+'''This script connects to a MySQL database and lists all of the
+    states from the database hbtn_0e_0_usa
+'''
 import MySQLdb
 import sys
 
