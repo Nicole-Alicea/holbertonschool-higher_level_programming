bbb31bd4ca0c20414fb2055ab83cbc0605a216a8
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-03-19 23:31:16 -0400

    task 17 chmod

--- python-object_relational_mapping/102=relationship_cities_states_list.py
+++ python-object_relational_mapping/102=relationship_cities_states_list.py
@@ -13,7 +13,7 @@ import sys
 if __name__ '__main__':
     db_uri = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
                                                             sys.argv[1],
-                                                            sys.argv[2],
+                                                            sys.argv[2], 
                                                             sys.argv[3])
     engine = create_engine(db_uri, pool_pre_ping=True)
     Base.metadata.create_all(engine)
