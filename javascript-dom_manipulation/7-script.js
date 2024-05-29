bdad4ef730f887eb0473920e6a2e679676ed6167
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-05-28 23:04:53 -0400

    task 7

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const listMoviesElement = document.getElementById('list_movies');
+
+  fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+    .then(response => response.json())
+    .then(data => {
+      const movies = data.results;
+      movies.forEach(movie => {
+        const listItem = document.createElement('li');
+        listItem.textContent = movie.title;
+        listMoviesElement.appendChild(listItem);
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+});
